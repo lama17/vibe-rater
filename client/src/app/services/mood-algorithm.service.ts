3517e10441177b06e0beee09cf67c246a34bcc96
Rava Rahbari <ravarahbari@Ravas-MBP.socal.rr.com>
2020-12-16 21:46:00 -0800

    Completed most of moodAlgo service

--- client/src/app/services/mood-algorithm.service.ts
+++ client/src/app/services/mood-algorithm.service.ts
@@ -8,71 +8,182 @@ import { SpotifyService } from './spotify.service';
 @Injectable({
   providedIn: 'root'
 })
-export class MoodAlgorithmService {
-  static moods = ['happy', 'sad', 'energetic', 'calm']
 
-  constructor(private spotifyService: SpotifyService) { }
+
+export class MoodAlgorithmService {
+  static moods = ['happy', 'sad', 'energetic', 'calm'];
+  trackList: TrackData[];
+  //featuresList = [];
+
+
+  constructor(private spotifyService: SpotifyService) {
+
+  }
 
   // PUBLIC FUNCTIONS - THESE 3 FUNCTIONS ARE THE ONLY ONES THAT SHOULD BE CALLED OUTSIDE THIS CLASS
-  ratePlaylist(playlist: PlaylistData): Object {
+  async ratePlaylist(playlist: PlaylistData): Promise<any> {
     // given a playlist, rate the playlist by its moods and return an object of its rankings. for example: [{love: 40}, {happy: 20}, {sad: 20}]
-    this.getAudioFeaturesForTracks(this.getTracksFromPlaylist(playlist.id));
-    return;
+    let moodList = await this.getAudioFeaturesForTracks(playlist.id);
+
+    var energy = 0;
+    var sadness = 0;
+    var calmness = 0;
+    var happiness = 0;
+
+    for (var i = 0; i < moodList.length; i++) {
+      if (moodList[i].mood == "Energetic") {
+        energy++;
+      }
+      else if (moodList[i].mood == "Sad") {
+        sadness++;
+      }
+      else if (moodList[i].mood == "Calm") {
+        calmness++;
+      }
+      else {
+        happiness++;
+      }
+    }
+    var results =
+    {
+      energetic: energy,
+      sad: sadness,
+      calm: calmness,
+      happy: happiness
+    }
+    console.log("rate playlists called");
+    console.log(results);
+    return results;
   }
 
-  buildPlaylist(mood: string, library: TrackData[]): PlaylistData {
+  async buildPlaylist(vibe: string): Promise<any> {
     // given a list of songs (library) and a mood (mood), return a playlist that contains tracks that match the mood. 
-    return;
-  }
+    // return list 
 
-  comparePlaylists(playlist1: PlaylistData, playlist2: PlaylistData): Object {
-    // given 2 playlists, rate each playlist using the ratePlaylist funciton, then return an object containing an average of the two playlists' rankings.
-    return;
-  }
-
-  // getMood(trackAudioFeatures:string[])
-  // {
-  //   let mood = "";
-  // }
-
-  // PRIVATE FUNCTIONS - SHOULD ONLY BE ACCESSIBLE BY OTHER FUNCTIONS IN THIS CLASS - make them public to test them tho //
-  private getTracksFromPlaylist(playlistId:string):any{
-    this.spotifyService.getTracksFromPlaylist(playlistId).then(data => {
-      console.log(data);
-      return data;
-    });
-  }
-  
-  getAudioFeaturesForTracks(tracks: TrackData[]): any {
-    // given a list of tracks, get the IDs and call getAudioFeaturesforMultipleTracks() from spotifyService and returns an array of track-feature items.
+    let p = await this.spotifyService.getMyLibrary();
     let idList = [];
-    tracks.forEach(track => {
-      idList.push(track.id);
-    });
-    this.spotifyService.getAudioFeaturesForMultipleTracks(idList).then(data => {
-      console.log('mood-algorithm.ts called getAudioFeatresForTracks')
-      console.log(data)
-      // map this data onto an object, then return it
 
-      var featuresArray = [];
+    for (var i of p) {
+      idList.push(i.id);
+    }
 
-      for (let i = 0; i < idList.length; i++) {
-        var trck =
-        {
-          id: data['audio_features'][i].id,
-          danceability: data['audio_features'][i].danceability,
-          energy: data['audio_features'][i].energy,
-          acousticness: data['audio_features'][i].acousticness,
-          valence: data['audio_features'][i].valence
-        }
-        featuresArray.push(trck);
+    let data = await this.spotifyService.getAudioFeaturesForMultipleTracks(idList);
+    var f = [];
+    let mood = "";
+    for (let i = 0; i < data.length; i++) {
+
+
+      if (data[i].danceability <= .5 || data[i].energy <= .4 || data[i].valence <= .3 || data[i].acousticness >= .5) {
+        mood = "Sad";
+      }
+      else if (data[i].danceability <= .4 || data[i].energy <= .2 || data[i].valence <= .2 || data[i].acousticness >= .7) {
+        mood = "Calm";
+      }
+      else if (data[i].danceability > .5 || data[i].energy > .85 || data[i].valence > .4 || data[i].acousticness <= .1) {
+        mood = "Energetic";
+      }
+      else if (data[i].danceability >= .6 || data[i].energy >= .7 || data[i].valence >= .5 || data[i].acousticness <= .2) {
+        mood = "Happy";
+      }
+      else {
+        mood = "Unable to Classify";
       }
 
-      console.log("Features array: ")
-      console.log(featuresArray);
+      var trck =
+      {
+        id: data[i].id,
+        mood: mood
+      }
 
-      return featuresArray;
+      f.push(trck);
+    }
 
-    })
+    let final = [];
+
+    for (let i = 0; i < f.length; i++)
+    {
+      if (f[i].mood == vibe)
+      {
+        var t = await this.spotifyService.getTrack(f[i].id);
+        final.push(t);
+      }
+    }
+
+    console.log("Build Playlist called:");
+    //console.log(f);
+    console.log(final);
+
+    return final;
+  }
+
+  async comparePlaylists(playlist1: string, playlist2: string): Promise<any> {
+    // given 2 playlists, rate each playlist using the ratePlaylist funciton, then return an object containing an average of the two playlists' rankings.
+
+    let p1 = await this.spotifyService.getPlaylist(playlist1);
+    let p2 = await this.spotifyService.getPlaylist(playlist2);
+
+    let p1Ident = await this.ratePlaylist(p1);
+    let p2Ident = await this.ratePlaylist(p2);
+
+    var energyAvg = (p1Ident.energetic + p2Ident.energetic) / 2;
+    var sadAvg = (p1Ident.sad + p2Ident.sad) / 2;
+    var calmAvg = (p1Ident.calm + p2Ident.calm) / 2;
+    var happyAvg = (p1Ident.happy + p2Ident.happy) / 2;
+
+    var results =
+    {
+      energetic: energyAvg,
+      sad: sadAvg,
+      calm: calmAvg,
+      happy: happyAvg
+    }
+    console.log("comparePlaylistResults: ")
+    console.log(results);
+    return results;
+  }
+
+
+  async getAudioFeaturesForTracks(playlistId: string): Promise<any> {
+    // given a list of tracks, get the IDs and call getAudioFeaturesforMultipleTracks() from spotifyService and returns an array of track-feature items.
+    let idList = [];
+    var mytracklist = await this.spotifyService.getTracksFromPlaylist(playlistId);
+
+    //console.log(mytracklist);
+    for (var t of mytracklist) {
+      idList.push(t.id);
+    }
+    let data = await this.spotifyService.getAudioFeaturesForMultipleTracks(idList);
+    var f = [];
+    let mood = "";
+    for (var i = 0; i < data.length; i++) {
+
+
+      if (data[i].danceability <= .5 || data[i].energy <= .4 || data[i].valence <= .3 || data[i].acousticness >= .5) {
+        mood = "Sad";
+      }
+      else if (data[i].danceability <= .4 || data[i].energy <= .2 || data[i].valence <= .2 || data[i].acousticness >= .7) {
+        mood = "Calm";
+      }
+      else if (data[i].danceability > .5 || data[i].energy > .85 || data[i].valence > .4 || data[i].acousticness <= .1) {
+        mood = "Energetic";
+      }
+      else if (data[i].danceability >= .6 || data[i].energy >= .7 || data[i].valence >= .5 || data[i].acousticness <= .2) {
+        mood = "Happy";
+      }
+      else {
+        mood = "Unable to Classify";
+      }
+
+      var trck =
+      {
+        id: data[i].id,
+        mood: mood
+      }
+
+      f.push(trck);
+
+      //  f.push(trck);
+    }
+    return f;
   }
 }
